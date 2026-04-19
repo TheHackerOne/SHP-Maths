6359f471286d83de0471889fa052e69b778ca962
Yash Chaudhary <43692087+TheHackerOne@users.noreply.github.com>
2026-04-19 15:49:48 +0530

    Update service-worker.js with v2

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,24 @@
-const CACHE_NAME = "mental-math-v1";
-const ASSETS = [
+const CACHE_NAME = "math-trainer-v2";
+
+const urlsToCache = [
   "./",
   "./index.html",
   "./style.css",
-  "./script.js",
-  "./manifest.json"
+  "./script.js"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
